f36b245779662c1fe8909f62c94f67ab20156fd2
Alexander Georgiev <alexandur04@abv.bg>
2019-03-16 18:49:35 +0200

    Added Scripts for riddles,box coliders for living room

---
+++ Assets/Scripts/ColidingToTable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColidingToTable : MonoBehaviour
+{
+    public GameObject ObjtoDestroy;
+    public GameObject ObjToAdd;
+    public GameObject Character;
+    public GameObject ObjToAdd2;
+    public GameObject TheObj;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject == TheObj)
+        {
+            ObjToAdd.SetActive(true);
+            ObjToAdd2.SetActive(true);
+            ObjtoDestroy.SetActive(false);
+
+
+
+        }
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/neshto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class neshto : MonoBehaviour
+{
+    public GameObject TheObjectToDestroy;
+    public GameObject TheObjectToDestroy2;
+    public GameObject TheObjectToEnable;
+    public GameObject TheObject;
+    public GameObject Player;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject == TheObject)
+        {
+            TheObjectToEnable.SetActive(true);
+            TheObjectToDestroy2.SetActive(false);
+
+
+
+        }
+        
+
+    }
+
+    void OnCollisionExit2D(Collision2D collisionex)
+    {
+        if (collisionex.gameObject == TheObject) {
+
+            TheObjectToDestroy.SetActive(false);
+        }
+
+    }
+
+
+}
+
+
+
