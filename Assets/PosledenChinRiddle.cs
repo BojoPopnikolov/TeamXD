719a2a84ab70dd99549a1dabcb4360a27103c1b6
Alexander Georgiev <alexandur04@abv.bg>
2019-03-17 00:39:14 +0200

    adding some scripts for character and for the riddles

---
+++ Assets/PosledenChinRiddle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosledenChinRiddle : MonoBehaviour
+{
+    public GameObject Character;
+    public GameObject TheObjectToColideWith;
+    public GameObject TheObjectToDestroy;
+    public GameObject TheObjectToAdd;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject == TheObjectToColideWith)
+        {
+            TheObjectToAdd.SetActive(true);
+            TheObjectToDestroy.SetActive(false);
+
+
+        }
+    }
+    
+    void OnCollisionExit2D(Collision2D colex)
+            {
+        if (colex.gameObject == TheObjectToColideWith)
+                {
+            TheObjectToAdd.SetActive(false);
+            
+
+
+    }
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
